c2fcf2c381e159a48c70fb13efb8ff2a939af146
gxcsoccer <gxcsoccer@126.com>
2016-11-10 22:12:24 +0800

    fix: use heartbeat logic to handle that connection stuck in CLOSE_WAIT status

---
+++ test/close_wait.test.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const should = require('should');
+const pedding = require('pedding');
+const TCPBase = require('../');
+const server = require('./support/server_end');
+
+describe('test/close_wait.test.js', () => {
+
+  class Client extends TCPBase {
+    constructor(options) {
+      Object.assign(options, {
+        headerLength: 8,
+        heartbeatInterval: 3000,
+      });
+
+      super(options);
+    }
+
+    getBodyLength(header) {
+      return header.readInt32BE(0);
+    }
+
+    decode(buf, header) {
+      let data;
+      if (buf) {
+        data = JSON.parse(buf);
+      }
+      return {
+        id: header.readInt32BE(4),
+        data,
+      };
+    }
+
+    get heartBeatPacket() {
+      return makeRequest(1).data;
+    }
+  }
+
+  function makeRequest(id, content, oneway) {
+    const header = new Buffer(8);
+    header.fill(0);
+    const body = new Buffer(JSON.stringify(content || {
+      id,
+      message: 'hello',
+    }));
+    header.writeInt32BE(body.length, 0);
+    header.writeInt32BE(id, 4);
+    return {
+      id,
+      oneway,
+      data: Buffer.concat([ header, body ]),
+    };
+  }
+
+  describe('end by server', () => {
+    before(done => server.start(9600, true, done));
+    after(() => server.close());
+
+    it('should close if end by server', done => {
+      const client = new Client({
+        host: '127.0.0.1',
+        port: 9600,
+      });
+      client.on('close', done);
+    });
+  });
+
+  describe('long time no response', () => {
+    before(done => server.start(9600, false, done));
+    after(() => server.close());
+
+    it('should close if long time no response', done => {
+      done = pedding(done, 2);
+      const client = new Client({
+        host: '127.0.0.1',
+        port: 9600,
+      });
+      client.on('close', done);
+      client.on('error', err => {
+        should.exist(err);
+        err.name.should.eql('ServerNoResponseError');
+        err.message.should.match(/server no response in \d+ms, maybe the socket is end on the other side/i);
+        done();
+      });
+    });
+  });
+
+});
